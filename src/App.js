d56b613521c16174a7a74aabcd6511127c02ea47
Josh Calder <josh@opensaas.com.au>
2022-02-17 08:25:22 +1100

    add route

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import "./App.css";
 
 // fontawesome
 import initFontAwesome from "./utils/initFontAwesome";
+import Fl0Api from "./views/Fl0Api";
 initFontAwesome();
 
 const App = () => {
@@ -38,6 +39,7 @@ const App = () => {
             <Route path="/" exact component={Home} />
             <Route path="/profile" component={Profile} />
             <Route path="/external-api" component={ExternalApi} />
+            <Route path="/fl0-api" component={Fl0Api} />
           </Switch>
         </Container>
         <Footer />
